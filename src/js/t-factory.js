0bc3aabc694a7138caafc1dabe7eec1b44d8e88a
Sebastian Seilund <sebastian@seilund.com>
2013-11-06 16:26:15 -0800

    fixed issue with multiple html entities not being replaced

--- src/js/t-factory.js
+++ src/js/t-factory.js
@@ -12,7 +12,7 @@ module.exports = function(prefix) {
         var s = Ember.I18n.t(prefix+key, context);
         for (var html in unescape) {
             if (!unescape.hasOwnProperty(html)) continue;
-            s = s.replace(html, unescape[html]);
+            s = s.replace(new RegExp(html, 'g'), unescape[html]);
         }
         return s;
     }
