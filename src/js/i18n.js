159f74880648d206319613c31cbf2e5c3fe22eb8
Sebastian Seilund <sebastian@seilund.com>
2013-11-21 12:30:22 -0800

    added support for change listeners

--- src/js/i18n.js
+++ src/js/i18n.js
@@ -3,13 +3,16 @@ require('ember-i18n');
 var storage = require('storage'),
     i18nContext = require('i18n-context');
 
-var currentLocale,
-    mainContext;
+var currentLocale = null,
+    mainContext = null,
+    changeListeners = [];
 
 module.exports = {
     init: init,
     destroy: destroy,
     locale: locale,
+    addChangeListener: addChangeListener,
+    removeChangeListener: removeChangeListener,
     t: t,
     tProperty: i18nContext.tProperty
 };
@@ -20,36 +23,65 @@ function init(localesPath) {
 }
 
 function destroy() {
-    mainContext.destroy();
+    if (mainContext) {
+        mainContext.destroy();
+    }
+    mainContext = currentLocale = null;
+    changeListeners = [];
+    i18nContext.setAllLocales('en_US');
 }
 
 function locale(newLocale) {
     if (arguments.length === 1) {
-        try {
-            loadLocale(newLocale);
+        if (newLocale !== currentLocale) {
+            try {
+                loadLocale(newLocale);
+            } catch (e) {
+                if (newLocale !== 'en_US') {
+                    console.error('Could not load locale `'+newLocale+'`. Falling back to `en_US`. Error stack:');
+                    console.log(e.stack);
+                    currentLocale = null;
+                    return locale('en_US');
+                } else {
+                    throw e;
+                }
+            }
+            
             currentLocale = newLocale;
             storage('locale', newLocale);
-        } catch (e) {
-            if (newLocale !== 'en_US') {
-                console.error('Could not load locale `'+newLocale+'`. Falling back to `en_US`. Error stack:');
-                console.log(e.stack);
-                return locale('en_US');
-            } else {
-                throw e;
-            }
+    
+            changeListeners.forEach(function(listener) {
+                listener.call(null, newLocale, toShort(newLocale));
+            });
         }
     }
+    
     return currentLocale;
 }
 
 function loadLocale(newLocale) {
-    var shortLocale = newLocale.substring(0, 2);
-
-    CLDR.defaultLanguage = shortLocale;
+    CLDR.defaultLanguage = toShort(newLocale);
 
     i18nContext.setAllLocales(newLocale);
 }
 
+function addChangeListener(listener) {
+    changeListeners.push(listener);
+}
+
+function removeChangeListener(listener) {
+    for (var i = 0; i < changeListeners.length; i++) {
+        if (changeListeners[i] === listener) {
+            changeListeners.splice(i, 1);
+            break;
+        }
+    }
+}
+
 function t(key, context) {
     return mainContext.t(key, context);
+}
+
+function toShort(newLocale) {
+    return newLocale.substring(0, 2);
 }
--- tests/i18n-test.js
+++ tests/i18n-test.js
@@ -2,7 +2,11 @@ var i18nContext = require('i18n-context'),
     storage = require('storage'),
     i18n = require('../src/js/i18n');
 
-var componentContext;
+var componentContext,
+    callsA = [],
+    callsB = [],
+    listenerA = function(locale, shortLocale) { callsA.push([locale, shortLocale]); },
+    listenerB = function(locale, shortLocale) { callsB.push([locale, shortLocale]); };
 
 QUnit.module('i18n', {
     setup: function() {
@@ -10,8 +14,13 @@ QUnit.module('i18n', {
     },
     teardown: function() {
         i18n.destroy();
+        
+        callsA = [];
+        callsB = [];
+        
         componentContext.destroy();
-        storage.testTeardown();
+        
+        storage.remove('locale');
     }
 });
 
@@ -25,6 +34,8 @@ function assertEnUs() {
 
     equal(componentContext.locale(), 'en_US');
     equal(componentContext.t('save'), 'Save');
+
+    equal(storage('locale'), 'en_US');
 }
 
 function assertDaDk() {
@@ -33,6 +44,8 @@ function assertDaDk() {
 
     equal(componentContext.locale(), 'da_DK');
     equal(componentContext.t('save'), 'Gem');
+
+    equal(storage('locale'), 'da_DK');
 }
 
 test('init() defaults to en_US', function() {
@@ -65,10 +78,76 @@ test('Set locale', function() {
 
 test('Set locale to invalid locale falls back to en_US', function() {
     init();
-    
+
     i18n.locale('te_ST');
 
     assertEnUs();
+});
+
+test('Listener is only called when locale actually changes', function() {
+    i18n.addChangeListener(listenerA);
+
+    init();
+
+    deepEqual(callsA, [['en_US', 'en']]);
+
+    i18n.locale('en_US');
+    i18n.locale('en_US');
+
+    deepEqual(callsA, [['en_US', 'en']]);
+});
+
+test('Listener is not called with invalid locale', function() {
+    init();
+
+    i18n.addChangeListener(listenerA);
+
+    i18n.locale('te_ST');
+
+    deepEqual(callsA, [['en_US', 'en']]);
+});
+
+test('Multiple listeners are called', function() {
+    i18n.addChangeListener(listenerA);
+    i18n.addChangeListener(listenerB);
+
+    init();
+
+    deepEqual(callsA, [['en_US', 'en']]);
+    deepEqual(callsB, [['en_US', 'en']]);
+
+    i18n.locale('da_DK');
+
+    deepEqual(callsA, [['en_US', 'en'], ['da_DK', 'da']]);
+    deepEqual(callsB, [['en_US', 'en'], ['da_DK', 'da']]);
+});
+
+test('Listeners are removed', function() {
+    i18n.addChangeListener(listenerA);
+    i18n.addChangeListener(listenerB);
+    i18n.removeChangeListener(listenerB);
+
+    init();
+
+    deepEqual(callsA, [['en_US', 'en']]);
+    deepEqual(callsB, []);
+
+    i18n.removeChangeListener(listenerA);
+    
+    i18n.locale('da_DK');
+
+    deepEqual(callsA, [['en_US', 'en']]);
+    deepEqual(callsB, []);
+});
+
+test('listeners are not fired for invalid locales', function() {
+    init();
+
+    i18n.addChangeListener(listenerA);
+
+    i18n.locale('te_ST');
+
+    deepEqual(callsA, [['en_US', 'en']]);
 });
 
 test('tProperty comes from i18n-context', function() {
